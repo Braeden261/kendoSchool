4bd543095553f9d822d11357c6bc13d031f4017b
Braeden Timlin <timl0008@algonquinlive.com>
2019-01-18 10:12:30 -0500

    Updated ReadMe, added folders, app.js file, index.html file, and javascript files

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # kendoSchool
 WebVR project for IMD3901B - Design Studio 3
+
+Setup:
+
+Pre-Requisite Software:
+
+Ensure the following software is installed on your computer.
+
+GitHub (Git Bash) https://desktop.github.com/
+
+Visual Studio Code https://code.visualstudio.com/
+
+Node.js https://nodejs.org/en/download/
+
+Set Up:
+
+After install the above software run the following commands:
+
+npm init --yes
+npm install express --save
+Ensure you are inside the Repo folder on your local machine when executing commands that pertain to the application.
+
+To launch application using localhost use the following command in git bash: node app.js
+
+on first launch of the application the Windows FireWall will likely prompt you to allow access, click Allow Access
+
+Open a web browser and enter the URL: localhost:8080
---
+++ app.js
@@ -0,0 +1,26 @@
+//consts for setting up server isntance
+//const is a constant
+//user let for variables you want to change
+//DO NOT USER var
+const express   = require('express');
+//let express = ???
+
+const app       = express();
+const http      = require("http");
+const server    = http.createServer(app);
+
+//default port is 80
+//default port for https is 443
+const LISTEN_PORT = 8080;
+
+//use middleware
+//these functions are acted upon the page before it is "served"
+//this is telling the server to assume the public folder where all html/js is found
+app.use(express.static(__dirname + '/public'));
+
+//create a "route" for accessing this page
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + 'public/index.html'); //send/serve this index.html file
+});
+server.listen(LISTEN_PORT);
+console.log('Listening to port' + LISTEN_PORT);
