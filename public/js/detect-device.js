6e225961ed3d1f706eab4acb2f939fd2f8bd8098
unknown <braedentimlin@cmail.carleton.ca>
2019-02-26 20:40:06 -0500

    Device Detection and redirection to correct page complete

--- public/js/detect-device.js
+++ public/js/detect-device.js
@@ -1,16 +1,19 @@
-'use strict';
-
-AFRAME.registerComponent('detect-device-component', {
-    init:function(){
-        const Context_AF = this;
-
-    },
-    detectDevice: function(){
-        if (AFRAME.utils.isMobile()){
-            //Mobile
-        }
-        else if (AFRAME.utils.checkHeadsetConnected()){
-            //Headset
-        }
+function detectDevice(){
+    if (AFRAME.utils.isMobile()){
+        //Mobile
+        console.log("Mobile");
+        window.location.href = '/master.html';
     }
-});
+    else if (AFRAME.utils.checkHeadsetConnected()){
+        //Headset
+        console.log("Headset");
+        window.location.href = '/student.html';
+
+    }
+    else if (!AFRAME.utils.checkHeadsetConnected()){
+        //Headset
+        console.log("Desktop");
+        window.location.href = '/student-test.html';
+
+    }
+}
