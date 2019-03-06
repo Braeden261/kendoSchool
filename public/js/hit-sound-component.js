2d78fbd7c00a71b63d8182362dbc54c4a9732ed5
Braeden Timlin <timl0008@algonquinlive.com>
2019-03-06 15:28:16 -0500

    Added Ambient Sounds

--- public/js/hit-sound-component.js
+++ public/js/hit-sound-component.js
@@ -4,22 +4,21 @@ AFRAME.registerComponent('hit-sound-component', {
     schema: {},
     init : function() {
         const Context_AF = this;
-        Context_AF.hitSound = document.querySelector('#hitSnd');
+        Context_AF.hitSound = document.querySelector('#hit-sound');
         Context_AF.el.addEventListener('collide', function (event) {
             //console.log(event.detail.target.velocity);
             let mag = Math.sqrt(Math.pow(event.detail.target.velocity.x, 2)+Math.pow(event.detail.target.velocity.y, 2)+Math.pow(event.detail.target.velocity.z, 2));
             mag *= 0.1;
+            if (mag < 0){
+                mag *= -1;
+            }
             if (mag > 1){
                 mag = 1;
             }
-            else if (mag < 0){
-                mag = 0;
-            }
-            if(mag > 0.05){
-                Context_AF.hitSound.volume = mag;
-                Context_AF.hitSound.components['sound'].playSound();
-                //console.log("Play Sound");
-            }
+            Context_AF.hitSound.volume = 1*mag;
+            Context_AF.hitSound.play();
+            console.log("Play Sound");
+            
         });
     }
 });
