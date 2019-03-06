200205252b063b6e90cce7afaf654afd0baa7fb4
Braeden Timlin <timl0008@algonquinlive.com>
2019-03-06 14:26:04 -0500

    Added Spatial Hit Sound Effect component

--- public/js/hit-sound-component.js
+++ public/js/hit-sound-component.js
@@ -5,10 +5,21 @@ AFRAME.registerComponent('hit-sound-component', {
     init : function() {
         const Context_AF = this;
         Context_AF.hitSound = document.querySelector('#hitSnd');
-        Context_AF.el.addEventListener('collide', function (e) {
-            //console.log('Sword has collided with body #' + e.detail.body.id);
-            Context_AF.hitSound.components['sound'].playSound();
-            console.log("Play Sound");
+        Context_AF.el.addEventListener('collide', function (event) {
+            //console.log(event.detail.target.velocity);
+            let mag = Math.sqrt(Math.pow(event.detail.target.velocity.x, 2)+Math.pow(event.detail.target.velocity.y, 2)+Math.pow(event.detail.target.velocity.z, 2));
+            mag *= 0.1;
+            if (mag > 1){
+                mag = 1;
+            }
+            else if (mag < 0){
+                mag = 0;
+            }
+            if(mag > 0.05){
+                Context_AF.hitSound.volume = mag;
+                Context_AF.hitSound.components['sound'].playSound();
+                //console.log("Play Sound");
+            }
         });
     }
 });
