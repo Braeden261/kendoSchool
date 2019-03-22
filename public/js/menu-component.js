f8a9c928fbafc245b1a08117177629ded703423f
Braeden Timlin <timl0008@algonquinlive.com>
2019-03-22 11:44:03 -0400

    removed extra stuff

--- public/js/menu-component.js
+++ public/js/menu-component.js
@@ -5,7 +5,6 @@ AFRAME.registerComponent('menu-component', {
     init : function() {
 
         const Context_AF = this;
-        Context_AF.hitSound = document.querySelector('#hit-sound');
         Context_AF.el.addEventListener('collide', function (event) {
             socket.emit(Context_AF.el.id);
         });
