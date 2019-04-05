929ef06a7618b97a9848a19a1bb589ac47b9f149
mattdono-CU <matthew.m.donoghue@gmail.com>
2019-04-05 16:35:36 -0400

    sword rack positioned out of the way of lanterns

--- public/js/menu-component.js
+++ public/js/menu-component.js
@@ -10,19 +10,21 @@ AFRAME.registerComponent('menu-component', {
 
         const Context_AF = this;
         Context_AF.el.addEventListener('collide', function (event) {
-            socket.emit(Context_AF.el.id);
-            setTimeout(function(){
-                Context_AF.el.setAttribute('event.detail.target.velocity', {x:0, y:0, z:0});
-                if (Context_AF.el.id == 'continue'){
-                Context_AF.el.setAttribute('position', {x:contPos[0], y:contPos[1], z:contPos[2]});
-                }
-                else if (Context_AF.el.id == 'repeat'){
-                    Context_AF.el.setAttribute('position', {x:repeatPos[0], y:repeatPos[1], z:repeatPos[2]});                    
-                }
-                else if (Context_AF.el.id == 'bow'){
-                    Context_AF.el.setAttribute('position', {x:bowPos[0], y:bowPos[1], z:bowPos[2]});                    
-                }
-            },1000);
+            if (event.detail.body.el.getAttribute('class') === 'sword') {
+                socket.emit(Context_AF.el.id);
+                setTimeout(function(){
+                    Context_AF.el.setAttribute('event.detail.target.velocity', {x:0, y:0, z:0});
+                    if (Context_AF.el.id == 'continue'){
+                    Context_AF.el.setAttribute('position', {x:contPos[0], y:contPos[1], z:contPos[2]});
+                    }
+                    else if (Context_AF.el.id == 'repeat'){
+                        Context_AF.el.setAttribute('position', {x:repeatPos[0], y:repeatPos[1], z:repeatPos[2]});                    
+                    }
+                    else if (Context_AF.el.id == 'bow'){
+                        Context_AF.el.setAttribute('position', {x:bowPos[0], y:bowPos[1], z:bowPos[2]});                    
+                    }
+                }, 1000);
+            }
         });
     }
 });
